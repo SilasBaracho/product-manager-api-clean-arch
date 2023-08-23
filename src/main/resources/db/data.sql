b39208bec7ad49083e17a224bb5662ad8cef0077
SilasBaracho <silasbaracho@gmail.com>
2023-08-23 20:30:41 -0300

    Inserts H2 Database

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,14 @@
+--Inserts INGREDIENT--
+INSERT INTO INGREDIENT(name, created_at) VALUES ('alface','2023-08-23 00:00:00');
+INSERT INTO INGREDIENT(name, created_at) VALUES ('cebola','2023-08-23 00:00:00');
+INSERT INTO INGREDIENT(name, created_at) VALUES ('tomate','2023-08-23 00:00:00');
+
+--Inserts PRODUCT_CATEGORY--
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('comida japonese','2023-08-23 00:00:00');
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('comida chinesa','2023-08-23 00:00:00');
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('hambúrguer americano','2023-08-23 00:00:00');
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('hambúrguer artesanal','2023-08-23 00:00:00');
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('hambúrguer gourmet','2023-08-23 00:00:00');
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('hambúrguer tradicional','2023-08-23 00:00:00');
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('hambúrguer vegano','2023-08-23 00:00:00');
+INSERT INTO PRODUCT_CATEGORY(name, created_at) VALUES ('hambúrguer vegetariano','2023-08-23 00:00:00');
